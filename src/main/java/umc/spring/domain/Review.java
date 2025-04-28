472c8fcafc3831c4085bb25c86f9bf9150f929bb
Jihyun <hjh20030615@gmail.com>
2025-04-29 02:02:29 +0900

    lucy_week5

---
+++ src/main/java/umc/spring/domain/Review.java
@@ -0,0 +1,24 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private Float score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")     // Review 테이블에 member_id 컬럼 생성
+    private Member member;
+}
